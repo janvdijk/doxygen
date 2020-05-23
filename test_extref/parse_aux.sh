7bc9803f276bff28453118a985723b5382581313
jan <jan@linux-a9ex>
2020-05-23 19:08:56 +0200

    Updated the dictval example.

--- test_extref/parse_aux.sh
+++ test_extref/parse_aux.sh
@@ -3,7 +3,7 @@ function make_dictionary
 {
 for AUXFILE in $@; do
   grep --no-filename "^\\\\newlabel" $@ |
-	sed 's/\\newlabel{\([^}]*\)}{{\([^}]*\)}{\([^}]*\)}[^$]*/\\extref \1#\2 on page \3/';
+	sed 's/\\newlabel{\([^}]*\)}{{\([^}]*\)}{\([^}]*\)}[^$]*/\1 \2/';
 done
 }
 
